7db369ce58b91adbeaa1bf1e9a952def50968804
201212 <v-aliali@expediagroup.com>
2022-08-04 13:18:30 +0530

    first commit

---
+++ src/main/java/com/hotel/pad/HotelAvailibilityPadApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.pad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAvailibilityPadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAvailibilityPadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/pad/HotelAvailibilityPadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.pad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAvailibilityPadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
