42fdce168ece6938789189491643a1f340d3e94e
apeethambaran <v-apeeth@expedia.com>
2022-08-29 12:44:18 +0530

    [UP][TECH-449] Service discovery changes for grpc server side

--- src/main/java/com/hotel/pad/HotelAvailibilityPadApplication.java
+++ src/main/java/com/hotel/pad/HotelAvailibilityPadApplication.java
@@ -2,7 +2,12 @@ package com.hotel.pad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.hotel.pad"})
 public class HotelAvailibilityPadApplication {
 
