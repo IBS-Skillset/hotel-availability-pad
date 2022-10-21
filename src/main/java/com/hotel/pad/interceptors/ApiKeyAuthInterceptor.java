0d9bbddcc7f0263c6f5bbd17032a4f021da48f26
Abhishek Ravindran <a-10749@ibsplc.com>
2022-10-21 09:50:31 +0530

    grpc Server interceptor

---
+++ src/main/java/com/hotel/pad/interceptors/ApiKeyAuthInterceptor.java
@@ -0,0 +1,31 @@
+package com.hotel.pad.interceptors;
+
+import io.grpc.*;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
+
+import java.util.Objects;
+
+
+@GrpcGlobalServerInterceptor
+@Slf4j
+public class ApiKeyAuthInterceptor implements ServerInterceptor {
+
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
+        log.info("Sever interceptor {}", serverCall.getMethodDescriptor());
+
+        Metadata.Key<String> apiKeyMetadata = Metadata.Key.of("accesstoken", Metadata.ASCII_STRING_MARSHALLER);
+        String apiKey = metadata.get(apiKeyMetadata);
+        log.info("accesstoken from client {}", apiKey);
+
+        if (Objects.nonNull(apiKey) && apiKey.equals("valuetest")) {
+            return serverCallHandler.startCall(serverCall, metadata);
+        } else {
+            Status status = Status.UNAUTHENTICATED.withDescription("Invalid accesstoken");
+            serverCall.close(status, metadata);
+        }
+        return new ServerCall.Listener<>() {
+        };
+    }
+}
